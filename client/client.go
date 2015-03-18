848fc364bf894106d65867ed635c02feb37fdccc
robcs621 <stevensr22@hotmail.com>
2015-03-18 17:25:16 -0400

    initial commit

---
+++ client/client.go
@@ -0,0 +1,97 @@
+/* CMSC-621
+Project 2
+client.go
+*/
+
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
+
+// client configuration object read in
+var conf ClientConfiguration
+
+type ClientConfiguration struct {
+	ServerID  string
+	Protocol  string
+	IpAddress string
+	Port      string
+	Methods   []string
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "Path to client configuration file")
+		os.Exit(1)
+	}
+	infile := os.Args[1]
+
+	// read in configuration file
+	fmt.Println("Loading client configuration...")
+	parseConfigurationFile(infile)
+
+	// connect to remote server
+	fmt.Print("Connecting to ", conf.ServerID, " at ", conf.IpAddress, " ... ")
+	conn, err := net.Dial(conf.Protocol, (conf.IpAddress + ":" + conf.Port))
+	checkErrorCondition(err)
+	fmt.Println("Success!")
+
+	// prompt for user input (assume properly formatted JSON message, with params "by-name")
+	var inputStr string
+	fmt.Println("===============================================================================================================")
+	fmt.Println("JSON messages entered must:")
+	fmt.Println(` -specify Server.[method name] in the "method" field`)
+	fmt.Println(` -pass arguments in the "params" field "by-name"`)
+	fmt.Println(`Example: {"method":"Server.Insert","params":[{ "Key":"keyA", "Rel":"relA", "Val":{"a":5, "b":6} }], "id":85}`)
+	fmt.Println("===============================================================================================================")
+	rdr := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("Please enter a properly formatted JSON message: ")
+
+		inputStr, err = rdr.ReadString('\n')
+		checkErrorCondition(err)
+
+		// send JSON message to server
+		fmt.Fprintf(conn, inputStr)
+		responseFromServer, err := bufio.NewReader(conn).ReadString('\n')
+		checkErrorCondition(err)
+
+		// display JSON response message from server
+		fmt.Println("\n")
+		fmt.Println("JSON message sent: ")
+		fmt.Println(inputStr)
+		fmt.Println("JSON message received:")
+		fmt.Println(responseFromServer)
+		fmt.Println("--------------------------------------------------------------------------------------------------------------")
+	}
+}
+
+func parseConfigurationFile(infile string) {
+	// read in client configuration
+	configurationFile, err := ioutil.ReadFile(infile)
+	checkErrorCondition(err)
+
+	// parse JSON in client configuration file
+	err = json.Unmarshal(configurationFile, &conf)
+	checkErrorCondition(err)
+
+	fmt.Println("   ServerID: ", conf.ServerID)
+	fmt.Println("   Protocol: ", conf.Protocol)
+	fmt.Println("   IP Address: ", conf.IpAddress)
+	fmt.Println("   Port: ", conf.Port)
+	fmt.Println("   Methods: ", conf.Methods)
+}
+
+func checkErrorCondition(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ node/node.go
@@ -0,0 +1,265 @@
+/* CMSC-621
+Project 2
+node.go
+*/
+
+package main
+
+import (
+	"encoding/gob"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"os"
+)
+
+// server configuration object read in
+var conf ServerConfiguration
+
+type ServerConfiguration struct {
+	ServerID                   string
+	Protocol                   string
+	IpAddress                  string
+	Port                       string
+	PersistentStorageContainer struct {
+		File string
+	}
+	Methods []string
+}
+
+type Server int
+
+type TripKey string
+type TripRel string
+type TripVal map[string]interface{}
+type KeyRelPair struct {
+	Key TripKey
+	Rel TripRel
+}
+
+type Args struct {
+	Key TripKey
+	Rel TripRel
+	Val TripVal // only used for insert and insertOrUpdate,
+}
+type LookupReply struct {
+	Key TripKey
+	Rel TripRel
+	Val TripVal
+}
+type InsertReply struct {
+	TripletInserted bool
+}
+type ListKeysReply struct {
+	KeyList []TripKey
+}
+type ListIDsReply struct {
+	IDList []KeyRelPair
+}
+
+// global variable
+var dict = map[KeyRelPair]TripVal{}
+
+// LOOKUP(keyA, relationA)
+func (t *Server) Lookup(args *Args, reply *LookupReply) error {
+
+	fmt.Print("  Lookup:    ", args.Key, ", ", args.Rel)
+
+	// construct temp KeyRelPair
+	krp := KeyRelPair{args.Key, args.Rel}
+
+	// return triplet Value if KeyRelPair exists
+	if tempVal, exists := dict[krp]; exists {
+		reply.Key = args.Key
+		reply.Rel = args.Rel
+		reply.Val = tempVal
+		fmt.Println(" ... Triplet found in DICT3.")
+	} else {
+		fmt.Println(" ... Triplet NOT found in DICT3.")
+	}
+	return nil
+}
+
+// INSERT(keyA, relationA, valA)
+func (t *Server) Insert(args *Args, reply *InsertReply) error {
+
+	fmt.Print("  Insert:      ", args.Key, ", ", args.Rel, ", ", args.Val)
+
+	// construct temp KeyRelPair
+	krp := KeyRelPair{args.Key, args.Rel}
+
+	// add key-rel pair if does not exist in dict
+	if _, exists := dict[krp]; !exists {
+		dict[krp] = args.Val
+		reply.TripletInserted = true // default is false
+		fmt.Println(" ... Does not exist in DICT3. Writing new triplet to disk.")
+		writeDictToDisk()
+	} else {
+		fmt.Println(" ... Triplet already exists in DICT3.")
+	}
+	return nil
+}
+
+// INSERTORUPDATE(keyA, relA, valA)
+func (t *Server) InsertOrUpdate(args *Args, reply *string) error {
+
+	fmt.Print("  InsOrUpd: ", args.Key, ", ", args.Rel, ", ", args.Val)
+
+	//construct temp KeyRelPair
+	krp := KeyRelPair{args.Key, args.Rel}
+	dict[krp] = args.Val
+
+	fmt.Println(" ... Writing new (or updated) triplet to disk.")
+	writeDictToDisk()
+
+	return nil
+}
+
+// DELETE(keyA, relA)
+func (t *Server) Delete(args *Args, reply *string) error {
+
+	fmt.Print("  Delete:     ", args.Key, ", ", args.Rel)
+
+	// construct temp KeyRelPair
+	krp := KeyRelPair{args.Key, args.Rel}
+	delete(dict, krp)
+	fmt.Println(" ... Removing deleted triplet from DICT3 and writing to disk.")
+	writeDictToDisk()
+	return nil
+}
+
+// LISTKEYS()
+func (t *Server) ListKeys(args *Args, reply *ListKeysReply) error {
+
+	fmt.Println("  ListKeys ")
+
+	// use map as a set of unique keys
+	var uniqueKeys = make(map[TripKey]bool)
+	var result []TripKey
+
+	// "Go Maps In Action" inspired: https://blog.golang.org/go-maps-in-action
+	for krp, _ := range dict {
+		if _, added := uniqueKeys[krp.Key]; !added {
+			uniqueKeys[krp.Key] = true
+			result = append(result, krp.Key)
+		}
+	}
+	reply.KeyList = result
+	return nil
+}
+
+// LISTIDs()
+func (t *Server) ListIDs(args *Args, reply *ListIDsReply) error {
+
+	fmt.Println("  ListIDs")
+
+	var ids []KeyRelPair
+	for krp, _ := range dict {
+		ids = append(ids, krp)
+	}
+	reply.IDList = ids
+	return nil
+}
+
+// SHUTDOWN()
+func (t *Server) Shutdown(args *Args, reply *string) error {
+
+	fmt.Println("  Shutting down ... ")
+	os.Exit(0)
+	return nil
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "Path to server configuration file")
+		os.Exit(1)
+	}
+	infile := os.Args[1]
+
+	// read in configuration file
+	fmt.Println("Loading server configuration...")
+	parseConfigurationFile(infile)
+
+	// open persistent storage container
+	fmt.Println("Accessing DICT3 persistent storage...")
+	openPersistentStorageContainer(conf.PersistentStorageContainer.File)
+
+	// start server
+	fmt.Println("Starting server ...")
+	tcpAddr, err := net.ResolveTCPAddr(conf.Protocol, ":"+conf.Port)
+	checkErrorCondition(err)
+
+	// register procedure call
+	rpc.Register(new(Server))
+
+	listener, err := net.ListenTCP(conf.Protocol, tcpAddr)
+	checkErrorCondition(err)
+	defer listener.Close()
+
+	fmt.Printf("Listening on port " + conf.Port + " ...\n")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go jsonrpc.ServeConn(conn)
+	}
+}
+
+func writeDictToDisk() {
+	// zero out existing storage container contents by creating new file
+	storageFile, err := os.Create(conf.PersistentStorageContainer.File)
+	checkErrorCondition(err)
+
+	// write map back to file
+	enc := gob.NewEncoder(storageFile)
+	enc.Encode(dict)
+	storageFile.Close()
+}
+
+func parseConfigurationFile(infile string) {
+	// read in server configuration
+	configurationFile, err := ioutil.ReadFile(infile)
+	checkErrorCondition(err)
+
+	// parse JSON in server configuration
+	err = json.Unmarshal(configurationFile, &conf)
+	checkErrorCondition(err)
+
+	fmt.Println("   ServerID: ", conf.ServerID)
+	fmt.Println("   Protocol: ", conf.Protocol)
+	fmt.Println("   IP Address: ", conf.IpAddress)
+	fmt.Println("   Port: ", conf.Port)
+	fmt.Println("   Persistent Storage Container: ", conf.PersistentStorageContainer.File)
+	fmt.Println("   Methods: ", conf.Methods)
+}
+
+func openPersistentStorageContainer(pathToStorageContainer string) {
+
+	// open file if it exists, create new file if not
+	storageFile, err := os.OpenFile(pathToStorageContainer, os.O_RDWR|os.O_CREATE, 0666)
+	checkErrorCondition(err)
+
+	// read back in binary data stored on disk to DICT3
+	dec := gob.NewDecoder(storageFile)
+	dec.Decode(&dict)
+	fmt.Println("   DICT3 contents stored on disk: ")
+	for k, v := range dict {
+		fmt.Println("    ", k, v)
+	}
+
+	// no need to keep file open until write back to disk at server shutdown
+	storageFile.Close()
+}
+
+func checkErrorCondition(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
