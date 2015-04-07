1eb453d100440e4ee8b734767727651addeb543e
Carter Waxman <cwaxman1@umbc.edu>
2015-04-07 14:04:30 -0400

    Added tmux to start_test_nodes_and_client.sh

--- start_test_nodes_and_client.sh
+++ start_test_nodes_and_client.sh
@@ -8,11 +8,19 @@ NUM_TEST_NODES_TO_RUN=4
 
 echo "Starting all ${NUM_TEST_NODES_TO_RUN} test nodes..."
 
+if [[ $TMUX == 1 ]]; then tmux new-session -d 'bash'; fi
+
 for ((node_num=$START; node_num<=$NUM_TEST_NODES_TO_RUN; node_num++))
 do
 	test_node_run_script_file_name=run_${TEST_NODE_DIR}${node_num}.sh
 
-	gnome-terminal --working-directory=${REPO_DIR}/${TEST_NODE_DIR}${node_num} -e ./${test_node_run_script_file_name} --window-with-profile=HOLD_OPEN --title="${TEST_NODE_DIR}${node_num}"
+    if [[ $TMUX == 1 ]]; then
+        pushd ${REPO_DIR}/${TEST_NODE_DIR}${node_num} &>/dev/null
+        tmux new-window -n "${TEST_NODE_DIR}${node_num}" ./${test_node_run_script_file_name}
+        popd &>/dev/null
+    else
+	    gnome-terminal --working-directory=${REPO_DIR}/${TEST_NODE_DIR}${node_num} -e ./${test_node_run_script_file_name} --window-with-profile=HOLD_OPEN --title="${TEST_NODE_DIR}${node_num}"
+    fi
 
 done
 
@@ -20,6 +28,12 @@ done
 sleep 4
 
 echo "Starting client..."
-gnome-terminal --working-directory=${REPO_DIR}/client -e ./run_client.sh --window-with-profile=HOLD_OPEN --title="CLIENT"
+if [[ $TMUX == 1 ]]; then
+    cd ${REPO_DIR}/client
+    tmux new-window -n "CLIENT" ./run_client.sh
+    tmux -2 attach-session -d
+else
+    gnome-terminal --working-directory=${REPO_DIR}/client -e ./run_client.sh --window-with-profile=HOLD_OPEN --title="CLIENT"
+fi
 
 
