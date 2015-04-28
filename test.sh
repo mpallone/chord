3eac229d658735a4f3073660737ec8216da8ea5d
Carter Waxman <cwaxman1@umbc.edu>
2015-04-28 15:58:31 -0400

    added stabilization

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir $GOPATH/{bin,lib} 2>/dev/null
+pkgs="
+    github.com/robcs621/proj2/chord
+    github.com/robcs621/proj2/client
+    github.com/robcs621/proj2/node
+"
+for p in $pkgs; do
+    echo Testing $p
+    go test $p || exit 1
+done
