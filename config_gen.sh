00fcc2c32f93a8916328b8f34e98416caffc7d52
emiller1 <elishiahmiller@yahoo.com>
2015-05-03 14:58:31 -0700

    added new config script

--- config_gen.sh
+++ config_gen.sh
@@ -2,6 +2,7 @@
 PROJPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 BIND=127.0.0.1
 BASE_PORT=7000
+PURGE_TIME="3/20/2015, 18:09:54"
 
 if [[ $# != 3 ]]; then
     echo Usage: $0 "<config_dir> <store_dir> <num_instances>"
@@ -24,7 +25,8 @@ for ((i=1; i<=$num_instances; i++)); do
 	"ipAddress" : "'$BIND'",
 	"port"	    : "'$PORT'",
 	"persistentStorageContainer" : {"file" : "'$store_dir/dict3_${i}.gob'"},
-	"methods"   : ["lookup", "insert", "insertOrUpdate", "delete", "listKeys", "listIDs", "shutdown"] 
+	"methods"   : ["lookup", "insert", "insertOrUpdate", "delete", "listKeys", "listIDs", "shutdown"], 
+	"purge"     : "'$PURGE_TIME'"
 }'  > $config_dir/${i}.config
 done
 cp $PROJPATH/client/client.config $config_dir/client.cfg
